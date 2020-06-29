390475e77d16e303eaa437ca6b011521616c047b
superj80820 <superj80820@gmail.com>
2020-06-29 11:29:24 +0800

    Add auth-service face-service

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const proxy = require('express-http-proxy')
+const authMeddleware = require('./middleware/auth')
 const app = express()
 
 app.use('/messfar-line-service', proxy('http://messfar-line-service:3002'))
+
+app.use('/auth-service', proxy('http://auth-service:3005'))
+
+app.use(
+  '/face-service',
+  authMeddleware.verifyToken,
+  proxy('http://messfar-line-service:3002')
+)
 
 app.listen(3003, function () {
   console.log('Listening on port 3003')
---
+++ scripts/pushToDockerhub.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build . -t=superj80820/api-gateway
+docker push superj80820/api-gateway
