79d040dcd4b2e514171d465b81bd68d81975ad3b
superj80820 <superj80820@gmail.com>
2020-07-03 01:48:12 +0800

    feat: Add file-service

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const proxy = require('express-http-proxy')
+const cors = require('cors')
 const authMeddleware = require('./middleware/auth')
 const app = express()
+
+app.use(cors())
 
 app.use('/messfar-line-service', proxy('http://messfar-line-service:3002'))
 
 app.use('/auth-service', proxy('http://auth-service:3005'))
 
 app.use(
+  '/file-service',
+  authMeddleware.verifyToken,
+  proxy('http://file-service:3001')
+)
+
+app.use(
   '/face-service',
   authMeddleware.verifyToken,
-  proxy('http://messfar-line-service:3002')
+  proxy('http://face-service:3000')
 )
 
 app.listen(3003, function () {
